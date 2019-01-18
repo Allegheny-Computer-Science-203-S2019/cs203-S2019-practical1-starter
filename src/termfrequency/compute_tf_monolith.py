172102fbbbf5a8fe028b56423b5322faeaa97074
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-01-18 12:29:27 -0500

    Add the template version of the compute_tf_monolith.py program.

---
+++ src/termfrequency/compute_tf_monolith.py
@@ -0,0 +1,40 @@
+import sys
+import string
+
+# pylint: disable=invalid-name
+word_freqs = []
+with open("stopwords/stop_words.txt") as f:
+    stop_words = f.read().split(",")
+stop_words.extend(list(string.ascii_lowercase))
+
+for line in open(sys.argv[1]):
+    start_char = None
+    i = 0
+    for c in line:
+        if start_char is None:
+            if c.isalnum():
+                start_char = i
+        else:
+            if not c.isalnum():
+                found = False
+                word = line[start_char:i].lower()
+                if word not in stop_words:
+                    pair_index = 0
+                    for pair in word_freqs:
+                        if word == pair[0]:
+                            pair[1] += 1
+                            found = True
+                            break
+                        pair_index += 1
+                    if not found:
+                        word_freqs.append([word, 1])
+                    elif len(word_freqs) > 1:
+                        for n in reversed(range(pair_index)):
+                            if word_freqs[pair_index][1] > word_freqs[n][1]:
+                                word_freqs[n], word_freqs[pair_index] = (
+                                    word_freqs[pair_index],
+                                    word_freqs[n],
+                                )
+                                pair_index = n
+                start_char = None
+        i += 1
